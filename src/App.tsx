df955308a43ccb5180b6ce93f760dc864609ff42
FROM-minyoung <918liter@gmail.com>
2023-03-12 00:50:20 +0900

    style: 기본 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function App() {
-  return <div className="">hi</div>;
+  return (
+    <div className="">
+      <header className="navbar">
+        <nav>
+          <ul>
+            <li>소개</li>
+            <li>프로젝트</li>
+            <li>스터디</li>
+            <li>연락처</li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <main>소개</main>
+        <main>프로젝트</main>
+        <main>스터디</main>
+        <main>연락처</main>
+      </section>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
