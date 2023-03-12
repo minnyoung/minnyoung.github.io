cea669476d80d2759cec3acb774372a77238562c
FROM-minyoung <918liter@gmail.com>
2023-03-13 01:59:46 +0900

    feature: 한 줄 소개 css 구현

--- src/components/Introduce.tsx
+++ src/components/Introduce.tsx
@@ -3,24 +3,36 @@ import React from "react";
 export default function Introduce() {
   const helpList = ["당신", "팀원", "사용자", "사람", "당신", "팀원", "사용자"];
   return (
-    <div className="h-[500px] mt-[60px] px-[30px] py-[100px] flex flex-col items-end font-[700] text-[45px] text-text-black">
-      <div>
-        <div className="absolute z-40 right-[490px] w-[130px] h-[68px] bg-white bg-opacity-90"></div>
-        안녕하세요
-      </div>
+    <main>
+      <article className="h-[500px] mt-[60px] px-[30px] py-[100px] flex flex-col items-end justify-center font-[700] text-[45px] text-text-black">
+        <div>
+          <div className="absolute z-40 right-[490px] w-[130px] h-[68px] bg-white bg-opacity-90"></div>
+          안녕하세요
+        </div>
 
-      <div className="absolute right-[490px] w-[130px] h-[200px] overflow-hidden">
-        {helpList.map((helper, index) => (
-          <p key={index} className="flex justify-end animate-up text-blue-600">
-            {helper}
-          </p>
-        ))}
-      </div>
-      <p className="">에게 도움이 되는 개발자,</p>
-      <div className="flex">
-        <div className="absolute right-[490px] w-[130px] h-[68px] bg-white bg-opacity-90"></div>
-        <p className="text-blue-600">권민영</p>입니다
-      </div>
-    </div>
+        <div className="absolute right-[490px] w-[130px] h-[200px] overflow-hidden">
+          {helpList.map((helper, index) => (
+            <p
+              key={index}
+              className="flex justify-end animate-up text-blue-600"
+            >
+              {helper}
+            </p>
+          ))}
+        </div>
+        <p>에게 도움이 되는 개발자,</p>
+        <div className="flex">
+          <div className="absolute right-[490px] w-[130px] h-[68px] bg-white bg-opacity-90"></div>
+          <p className="text-blue-600">권민영</p>입니다
+        </div>
+      </article>
+      <article className="justify-center text-center">
+        한줄 소개 예시1
+        <br />
+        한줄 소개 예시2
+        <br />
+        한줄 소개 예시3
+      </article>
+    </main>
   );
 }
